341e7688dd55e080f83b60da49fc6106492d147f
jnog93 <89908029+jnog93@users.noreply.github.com>
2021-10-29 15:44:55 +0100

    Update animated-background.js

--- dist/animated-background.js
+++ dist/animated-background.js
@@ -80,14 +80,16 @@ function getVars() {
     var theme_url = Hass.themes.themes[theme]["background-video"];
     var panel_selected = /homekit/; // a variavel declarada vai associar-se a todos os temas que estejam associados ao homekit
     var panel_selecte = Hass.panels.panels[theme]['/homekit/'];
+    console.log("Panel 1", panel_selected);
+    console.log("Panel 2", panel_selecte);
     
     //var panel_selected = Hass.panels.panels[theme][homekit]
     }
     catch{}
-    console.log("Chamar HomeKit", homekit);
+  
 
     //if (!Animated_Config && typeof theme_url !== 'undefined' && typeof panel_selected !== 'undefined' && typeof panel_selected !== 'undefined'){ //adicionado apartir do 1º 'undefined'
-    if (!Animated_Config && typeof theme_url != 'undefined' && typeof panel_selected != 'undefined'){
+    if (!Animated_Config && typeof theme_url !== 'undefined' && typeof panel_selected !== 'undefined'){
       //Animated_Config = {"default_url": theme_url, panel_selected, panel_selecte}; //adicionado panel_selected
       Animated_Config = {"default_url": theme_url, panel_selected}; //adicionado panel_selected
     }

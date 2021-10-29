4d2111d7f7f3e4b291c5c64748a6923485106388
jnog93 <89908029+jnog93@users.noreply.github.com>
2021-10-29 15:55:23 +0100

    Update animated-background.js

--- dist/animated-background.js
+++ dist/animated-background.js
@@ -85,13 +85,14 @@ function getVars() {
     
     //var panel_selected = Hass.panels.panels[theme][homekit]
     }
-    catch{}
+    catch{console.log("deu erros no try");}
   
 
     //if (!Animated_Config && typeof theme_url !== 'undefined' && typeof panel_selected !== 'undefined' && typeof panel_selected !== 'undefined'){ //adicionado apartir do 1º 'undefined'
     if (!Animated_Config && typeof theme_url !== 'undefined' && typeof panel_selected !== 'undefined'){
       //Animated_Config = {"default_url": theme_url, panel_selected, panel_selecte}; //adicionado panel_selected
       Animated_Config = {"default_url": theme_url, panel_selected}; //adicionado panel_selected
+      console.log("entrei aqui");
     }
 
     View_Layout = Root.shadowRoot.getElementById("layout");

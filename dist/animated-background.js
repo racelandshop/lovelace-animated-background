4f45f5a93edb5ccb43a3309995142135bd1224c1
jnog93 <89908029+jnog93@users.noreply.github.com>
2021-10-29 16:23:05 +0100

    Update animated-background.js

--- dist/animated-background.js
+++ dist/animated-background.js
@@ -86,7 +86,7 @@ function getVars() {
     }
     catch{console.log("deu erros no try", panel_selected);}
     
-    if (!Animated_Config && typeof theme_url != 'undefined' && typeof panel_selected != 'undefined'){
+    if (!Animated_Config && typeof theme_url !== 'undefined' && typeof panel_selected !== 'undefined'){
       Animated_Config = {"default_url": theme_url, panel_selected}; //adicionado panel_selected
       console.log("entrei aqui");
     }
